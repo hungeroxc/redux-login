dcf5c45503a56454a2ce007b2b1b21e56a762e8c
hfpp2012 <hfpp2012@gmail.com>
2018-09-07 18:52:34 +0800

    change bookshelf

--- server/bookshelf.js
+++ server/bookshelf.js
@@ -2,4 +2,7 @@ import knex from 'knex';
 import bookshelf from 'bookshelf';
 import knexConfig from '../knexfile';
 
-export default bookshelf(knex(knexConfig.development));
+const environment = process.env.NODE_ENV || 'development'
+console.log(environment);
+
+export default bookshelf(knex(knexConfig[environment]));
